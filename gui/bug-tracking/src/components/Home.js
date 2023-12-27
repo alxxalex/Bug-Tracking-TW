79a0a16bca2ca24d154ab39dc2d634ad91b593dc
Ovidiu Ra2 <112311269+OvidiuRa2@users.noreply.github.com>
2023-12-27 16:54:30 +0200

    modify home page

--- gui/bug-tracking/src/components/Home.js
+++ gui/bug-tracking/src/components/Home.js
@@ -1,22 +1,50 @@
-import { Link } from "react-router-dom";
 import "./Home.css";
+import { useEffect, useState } from "react";
+
+const SERVER = "http://localhost:5001";
 
 function Home() {
+  const [projects, setProjects] = useState([]);
+
+  const fetchProjects = async () => {
+    try {
+      const response = await fetch(`${SERVER}/api/projects`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const projectsData = await response.json();
+      setProjects(projectsData);
+    } catch (error) {
+      console.error('Error fetching projects:', error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchProjects();
+  }, []);
+
   return (
     <div className="home">
-      <div className="button-container">
-        <div className="signup-button">
-          <Link to="/signup">
-            <button>Sign up</button>
-          </Link>
-        </div>
-
-        <div className="login-button">
-          <Link to="/login">
-            <button>Log in</button>
-          </Link>
-        </div>
-      </div>
+      <button className="button">Add Project</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {projects.map((project, index) => (
+            <tr key={index}>
+              <td>{project.name}</td>
+              <td className="td-button">
+                <button className="button">Test</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
